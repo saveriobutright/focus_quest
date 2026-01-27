3d148510cce9edba34048b8f017c68150649be38
saveriobutright <pltsvr03d14i639l@studenti.unical.it>
2026-01-27 10:23:34 +0100

    Modelli, aggiunto UserModel e configurate dipendenze sqflite e riverpod nel pubspec

---
+++ lib/models/user_model.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  final String name;
+  final int level;
+  final int currentXp;
+
+  UserModel({
+    required this.name,
+    required this.level,
+    required this.currentXp,
+  });
+
+  //Missing XP to get to the next level
+  int get xpToNextLevel => level * 1000;
+
+  // Transform database data to a Dart object
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'],
+      level: map['level'],
+      currentXp: map['current_xp'],
+    );
+  }
+}
