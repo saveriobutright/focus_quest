0dc70946e3fc06cf1666839b693fa5c766f3ada1
saveriobutright <pltsvr03d14i639l@studenti.unical.it>
2026-01-31 10:13:28 +0100

    Feature: implementazione ThemeProvider e switch per Tema Scuro nella pagina Profilo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/home_page.dart';
+import 'providers/theme_provider.dart';
 
 void main(){
   //ProviderScope abilitates Riverpod everywhere
@@ -11,17 +12,21 @@ void main(){
   );
 }
 
-class FocusQuest extends StatelessWidget{
+class FocusQuest extends ConsumerWidget{
   const FocusQuest({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context, WidgetRef ref){
+    
+    final isDarkMode = ref.watch(themeProvider);
+
     return MaterialApp(
       title: 'FocusQuest',
       debugShowCheckedModeBanner: false,  
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.deepPurple,
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
       ),
       home: const HomePage(),
     );
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/theme_provider.dart';
+import '../providers/user_provider.dart';
 
-class ProfilePage extends StatelessWidget{
+class ProfilePage extends ConsumerWidget{
   
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Schermata profilo in arrivo"));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userAsync = ref.watch(userProvider);
+    final isDarkMode = ref.watch(themeProvider);
+
+    return Scaffold(
+      body: userAsync.when(
+        data: (user) => Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                child: Icon(Icons.person, size: 50),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                user.name,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Divider(height: 40),
+
+              SwitchListTile(
+                title: const Text("Tema Scuro"),
+                subtitle: const Text("Cambia l'aspetto dell'app"),
+                secondary: const Icon(Icons.dark_mode),
+                value: isDarkMode,
+                onChanged: (value){
+                  ref.read(themeProvider.notifier).state = value;
+                },
+              ),
+            ],
+          ),
+        ), 
+        error: (err, stack) => Center(child: Text('Errore: $err')), 
+        loading: () => const Center(child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
